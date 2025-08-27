d9e040fb3e2b0ea459c2463bdcc2144965491bf3
AshutoshSri123 <ashu.61003@gmail.com>
2025-08-28 02:36:04 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ashutosh Srivastava on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
